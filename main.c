63a9b8f5f902f5c935426b9a2bf33a3282c95e8a
adham077 <adham.walaa@gmail.com>
2024-11-14 18:16:11 +0200

    added mul feature

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "stdio.h"
 #include "conio.h"
-
+#include "multiplication.c"
 
 void scan_input(float*,float*);
 
@@ -15,9 +15,11 @@ int main(void){
         c = getch();
         
         switch(c){
-            
-           
-                
+
+            case '*':
+                scan_input(&a,&b);
+                printf("a*b=%d\n",multiplication(a,b));
+
             default:
                 printf("invalid operation\n");
 
