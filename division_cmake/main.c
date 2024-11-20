ca639c97a2e54f4245544bc55fcccf251c4b0e7a
mostafaelgendy99 <mostafa.elgendy76@gmail.com>
2024-11-20 13:05:00 +0200

    division_cmake_added

---
+++ division_cmake/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <conio.h>
+
+#incude "division.h"
+
+
+void scan_input(float*,float*);
+
+int main(void){
+    do{
+        char c;
+        float a,b;
+        printf("\033c");        
+
+        printf("please enter your preffered operation: + - * / %\n ");
+        flushall;
+        c = getch();
+        
+        switch(c){
+			
+	  
+           case '/':
+                scan_input(&a,&b);
+                printf("a*b=%d\n",Division(a,b));
+                break;
+
+          
+
+           default:
+                printf("invalid operation\n");
+
+        }
+
+        printf("press any key if you want to continue, for exit press n");
+        flushall;
+    }while(getch()!='n');
+
+    return 0;
+}
+
+void scan_input(float* a,float* b){
+    printf("enter a:\n");
+    fflush(stdin);fflush(stdout);
+    scanf("%f",a);
+    printf("enter b:\n");
+    fflush(stdin);fflush(stdout);
+    scanf("%f",b);
+}
---
+++ division_cmake/src/division.c
@@ -0,0 +1,15 @@
+#include "division.h"
+
+float Division (float x , float y)
+{
+
+
+if (y==0){
+    printf("please enter another number");
+}
+else {
+    return x/y ; 
+}
+
+
+}
